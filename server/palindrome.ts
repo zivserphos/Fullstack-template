cf41ee9cf01a2d2eff659f009b35c8cf2f0cf897
zivserphos <zivfromisrael@gmail.com>
2021-12-26 13:44:06 +0200

    delete unnecessary files

--- server/palindrome.ts
+++
@@ -1,8 +0,0 @@
-export const palindrome = (string: string) =>
-  string.split("").reverse().join("");
-
-export const average = (array: number[]) => {
-  const reducer = (sum: number, item: number) => sum + item;
-
-  return array.reduce(reducer, 0) / array.length;
-};
